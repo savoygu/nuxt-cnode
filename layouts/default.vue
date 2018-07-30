bb4b43013eb731cd7f4acf7e2de9a4efc3d9b900
savoygu <savoygu@gmail.com>
2018-07-30 20:32:30 +0800

    feat(navbar): add navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,45 @@
 <template>
   <div>
+    <div class="navbar">
+      <div class="navbar__inner">
+        <div class="navbar__container">
+          <div class="navbar__site">
+            <a class="navbar__brand">
+              <img src="//o4j806krb.qnssl.com/public/images/cnodejs_light.svg" alt="Cnode 中文社区">
+            </a>
+            <form class="navbar__search" action="/search">
+              <!-- <i class="cn-icon-search"></i> -->
+              <input type="text" id="q" name="q">
+            </form>
+          </div>
+          <ul class="navbar__nav">
+            <li><a href="/">首页</a></li>
+            <li><a href="/">新手入门</a></li>
+            <li><a href="/">API</a></li>
+            <li><a href="/">关于</a></li>
+            <li><a href="/">注册</a></li>
+            <li><a href="/">登录</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
     <nuxt/>
   </div>
 </template>
 
 <style lang="scss">
 @import "~/assets/theme/index.scss";
+/* =======================
+  CSS Reset
+ ======================= */
+@import "reset-css/sass/_reset.scss";
+
+@import "normalize-scss";
+@include normalize();
 
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  // font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: "Helvetica Neue","Luxi Sans","DejaVu Sans",Tahoma,"Hiragino Sans GB",STHeiti,sans-serif!important;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -16,6 +47,10 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+}
+
+body {
+  background-color: #e1e1e1;
 }
 
 *, *:before, *:after {
@@ -52,10 +87,102 @@ html {
   background-color: #35495e;
 }
 
-@include b(default) {
+@include b(navbar) {
+  position: relative;
+  z-index: 9;
+  width: 100%;
+  margin-bottom: 0;
+  font-size: 13px;
+  background: #444;
 
-  @include e(button) {
-    background-color:darkturquoise;
+  @include e(inner) {
+    width: 90%;
+    min-height: 50px;
+    margin: auto;
+    padding: 5px;
+    background: 0 0;
+    border: none;
+    border-radius: 0;
+    box-shadow: none;
   }
+
+  @include e(container) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    min-width: 960px;
+    margin: 0 auto;
+    max-width: 1400px;
+  }
+
+  @include e(site) {
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  @include e(brand) {
+    width: 160px;
+    padding: 3px 20px;
+    height: 40px;
+    line-height: 40px;
+    color: #ccc;
+    font-weight: 700;
+  }
+
+  @include e(search) {
+    position: relative;
+    display: flex;
+    align-items: center;
+
+    i {
+      position: absolute;
+      left: 3px;
+      color: #a5a6a5;
+    }
+
+    input {
+      width: 233px;
+      height: 26px;
+      vertical-align: middle;
+      padding: 3px 5px 3px 22px;
+      font-size: 13px;
+      font-weight: 400;
+      line-height: 1;
+      color: #666;
+      background: url(https://o4j806krb.qnssl.com/public/images/search.e53b380a.hashed.png) 4px 4px no-repeat #888;
+      background-color: #888;
+      border: 0;
+      border-radius: 15px;
+      transition: all .5s;
+
+      &:focus {
+        background-color: #fff;
+        border-color: rgba(82,168,236,.8);
+        outline: 0;
+        box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgba(82,168,236,.6);
+      }
+    }
+  }
+
+  @include e(nav) {
+    display: flex;
+    align-items: center;
+
+    a {
+      display: block;
+      padding: 10px 15px;
+      line-height: 20px;
+      text-decoration: none;
+      text-shadow: none;
+      color: #ccc;
+
+      &:hover {
+        color: #fff;
+      }
+    }
+  }
+
 }
+
 </style>
