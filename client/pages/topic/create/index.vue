2f1edbf8e3519248ac6f6e678114777248692e6e
savoygu <savoygu@gmail.com>
2018-08-06 10:33:00 +0800

    feat(editormd): integration editormd markdown editor

---
+++ client/pages/topic/create/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="main topic-create">
+    <div class="main__content">
+      <div class="main__panel">
+        <div class="main__header">
+          <breadcrumb>
+            <breadcrumb-item to="/">主页</breadcrumb-item>
+            <breadcrumb-item>发布话题</breadcrumb-item>
+          </breadcrumb>
+        </div>
+        <div class="topic-create__content">
+          <div class="topic-create__plate">
+            <select name="plate" id="plate" v-model="plate">
+              <option value="请选择"></option>
+              <option value="分享"></option>
+              <option value="问答"></option>
+              <option value="招聘"></option>
+              <option value="客户端测试"></option>
+            </select>
+          </div>
+          <div class="topic-create__title">
+            <input type="text" v-model="title">
+          </div>
+          <div class="topic-create__text">
+            <div id="editormd">
+                <textarea style="display:none;">### Hello Editor.md !</textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main__sidebar sidebar"></div>
+  </div>
+</template>
+
+<script>
+import Breadcrumb from '~/components/breadcrumb'
+import BreadcrumbItem from '~/components/breadcrumb/item'
+
+export default {
+  name: 'TopicCreate',
+
+  components: {
+    Breadcrumb,
+    BreadcrumbItem
+  },
+
+  data() {
+    return {
+      plate: '',
+      title: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@include b(topic-create) {
+  .CodeMirror, .editormd-preview {
+    height: 450px;
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -11,13 +11,11 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
-    script: [
-      { innerHTML: 'window.onload = function () { prettyPrint() }', type: 'text/javascript' }
     ]
   },
   css: [
     '~/assets/prettify/prettify.css',
+    '~/assets/editormd/css/editormd.css',
     { src: 'reset-css/sass/_reset.scss', lang: 'scss' },
     { src: '~/assets/theme/index.scss', lang: 'scss' }
   ],
@@ -44,7 +42,9 @@ module.exports = {
     },
     vendor: [
       '~/assets/prettify/prettify.js',
-      'axios'
+      '~/assets/jquery/jquery.js',
+      '~/assets/editormd/editormd.js',
+      'axios',
     ]
   },
   modules: [
@@ -72,7 +72,9 @@ module.exports = {
     '~/plugins/filters.js',
     '~/plugins/packages.js',
     { src: '~/assets/prettify/prettify.js', ssr: false },
-    { src: '~/plugins/localStorage.js', ssr: false }
+    { src: '~/plugins/localStorage.js', ssr: false },
+    { src: '~/assets/jquery/jquery.js', ssr: false },
+    { src: '~/assets/editormd/editormd.js', ssr: false }
   ],
   srcDir: 'client/',
   serverMiddleware: [
