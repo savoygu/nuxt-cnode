d9eef902f4b4c9650f419c57785026757984e603
savoygu <savoygu@gmail.com>
2018-08-13 17:25:37 +0800

    refactor(editor): change cdn to local file

--- client/pages/topic/_id/index.vue
+++ client/pages/topic/_id/index.vue
@@ -97,8 +97,10 @@ export default {
         { rel: 'stylesheet', href: '//cdn.jsdelivr.net/editor/0.1.0/editor.css' }
       ],
       script: [
-        { src: '//cdn.jsdelivr.net/editor/0.1.0/editor.js' },
-        { src: '//cdn.jsdelivr.net/editor/0.1.0/marked.js' }
+        { src: '/editor/editor.js' },
+        { src: '/editor/marked.js' }
+        // { src: '//cdn.jsdelivr.net/editor/0.1.0/editor.js' },
+        // { src: '//cdn.jsdelivr.net/editor/0.1.0/marked.js' }
       ]
     }
   },
