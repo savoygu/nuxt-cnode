65cec9e509430d76eaf0669b174d3e9f61ac9427
savoygu <savoygu@gmail.com>
2018-08-15 07:47:45 +0800

    chore(npm): add cors dependence

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const path = require('path')
 const fs = require('fs')
 
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(session({
