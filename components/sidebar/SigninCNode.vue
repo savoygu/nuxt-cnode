a6bddc276ae5a8ac1d833cf623e78e19c04974b1
savoygu <1029559886@qq.com>
2023-03-04 11:43:01 +0800

    feat: add SigninCNode sidebar

---
+++ components/sidebar/SigninCNode.vue
@@ -0,0 +1,18 @@
+<template>
+  <Panel :header="false">
+    <p class="signin-cnode__title">CNode：Node.js专业中文社区</p>
+    <div>
+      您可以通过 CNode Token
+      <NuxtLink to="/signin">登录</NuxtLink>
+    </div>
+  </Panel>
+</template>
+
+<style lang="scss">
+@include b(signin-cnode) {
+  @include e(title) {
+    margin-bottom: 10px;
+    font-size: 14px;
+  }
+}
+</style>
