df68f9a8b364bb9e27c37e88affcf146319ab0cc
savoygu <1029559886@qq.com>
2023-03-27 00:51:44 +0800

    feat: handle arbitrary path jumps to the user details

---
+++ pages/[slug].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  redirect: to => {
+    return {
+      name: 'user',
+      params: {
+        loginname: to.params.slug
+      }
+    }
+  }
+})
+</script>
--- pages/user/[loginname]/index.vue
+++ pages/user/[loginname]/index.vue
@@ -1,39 +1,60 @@
 <script setup lang="ts">
-// hooks
-const route = useRoute()
-const loginname = route.params.loginname as string
+definePageMeta({
+  name: 'user'
+})
 
-const { data: user } = await fetchUser(loginname)
+// hooks
+const state = useStore()
+const route = useRoute()
+const loginUser = computed(() => state.value.user)
+const loginname = route.params.loginname as string
+const { data: currentUser } = await fetchUser(loginname)
+const user = computed(() => currentUser.value ?? state.value.users[loginUser.value?.loginname ?? ''])
+if (!currentUser.value && loginUser.value) {
+  await fetchUser(loginUser.value.loginname)
+}
 </script>
 
 <template>
-  <TheMain v-if="user">
-    <Panel>
+  <TheMain>
+    <template v-if="currentUser">
+      <Panel>
+        <template #header>
+          <BaseBreadcrumb>
+            <BaseBreadcrumbItem to="/">主页</BaseBreadcrumbItem>
+            <BaseBreadcrumbItem></BaseBreadcrumbItem>
+          </BaseBreadcrumb>
+        </template>
+        <div class="user__info">
+          <div class="user__personal">
+            <img class="user__avatar" :src="user.avatar_url" alt="" />
+            <span class="user__name">{{ user.loginname }}</span>
+          </div>
+          <div class="user__profile">
+            <div class="user__score">{{ user.score }} 积分</div>
+            <div class="user__collection">
+              <nuxt-link :to="`/user/${user.loginname}/collections`">查看话题收藏</nuxt-link>
+            </div>
+          </div>
+          <p class="user__register">注册时间 {{ timeAgo(user.create_at) }}</p>
+        </div>
+      </Panel>
+      <Panel title="最近创建的话题">
+        <TopicLatest :topics="user.recent_topics"></TopicLatest>
+      </Panel>
+      <Panel title=" 最近参与的话题">
+        <TopicLatest :topics="user.recent_replies"></TopicLatest>
+      </Panel>
+    </template>
+    <Panel v-else>
       <template #header>
         <BaseBreadcrumb>
           <BaseBreadcrumbItem to="/">主页</BaseBreadcrumbItem>
-          <BaseBreadcrumbItem></BaseBreadcrumbItem>
+          <BaseBreadcrumbItem>通知</BaseBreadcrumbItem>
         </BaseBreadcrumb>
       </template>
-      <div class="user__info">
-        <div class="user__personal">
-          <img class="user__avatar" :src="user.avatar_url" alt="" />
-          <span class="user__name">{{ user.loginname }}</span>
-        </div>
-        <div class="user__profile">
-          <div class="user__score">{{ user.score }} 积分</div>
-          <div class="user__collection">
-            <nuxt-link :to="`/user/${user.loginname}/collections`">查看话题收藏</nuxt-link>
-          </div>
-        </div>
-        <p class="user__register">注册时间 {{ timeAgo(user.create_at) }}</p>
-      </div>
-    </Panel>
-    <Panel title="最近创建的话题">
-      <TopicLatest :topics="user.recent_topics"></TopicLatest>
-    </Panel>
-    <Panel title=" 最近参与的话题">
-      <TopicLatest :topics="user.recent_replies"></TopicLatest>
+      <BaseAlert :model-value="true" title="这个用户不存在。"></BaseAlert>
+      <NuxtLink to="/" class="button--gray user__back">返回</NuxtLink>
     </Panel>
     <template #sidebar>
       <SidebarPersonalInformation :user="user" />
@@ -84,6 +105,11 @@ const { data: user } = await fetchUser(loginname)
     color: #ababab;
     font-size: 14px;
     line-height: 2em;
+  }
+
+  @include e(back) {
+    margin-top: 20px;
+    text-decoration: none;
   }
 }
 
