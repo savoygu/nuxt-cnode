209288e0cd5106dd4c7aed42409600c110805ff5
savoygu <1029559886@qq.com>
2026-02-13 18:21:21 +0800

    refactor(router): 优化路由过滤逻辑

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,18 +1,14 @@
 import type { RouterConfig } from '@nuxt/schema'
-import type { RouteRecordRaw } from 'vue-router'
 
 export default {
   routes: (_routes) => {
-    const routes = _routes.reduce((acc, route) => {
+    const routes = _routes.filter((route) => {
       // 过滤掉 _ 开头的路径
       if (route.path.includes('/_')) {
-        return acc
+        return false
       }
-
-      acc.push(route)
-
-      return acc
-    }, [] as RouteRecordRaw[])
+      return true
+    })
 
     return [...routes]
   },
